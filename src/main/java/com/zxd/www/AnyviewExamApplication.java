1e4596eabe5ed66caafe909615faa6f6ffc09916
Makonike <1694585033@qq.com>
2021-10-10 09:53:22 +0800

    feat(all): init repository

---
+++ src/main/java/com/zxd/www/AnyviewExamApplication.java
@@ -0,0 +1,16 @@
+package com.zxd.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Makonike
+ */
+@SpringBootApplication
+public class AnyviewExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyviewExamApplication.class, args);
+    }
+
+}

8155c7123aea6b4b11a9a26229261a21b6c643f1
Makonike <1694585033@qq.com>
2021-10-15 22:47:28 +0800

    feat(sys): 添加教师相关操作接口，并删除部分冗余代码

---
+++ src/main/java/com/zxd/www/sys/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,59 @@
+package com.zxd.www.sys.service.impl;
+
+import com.zxd.www.sys.entity.SysAdminEntity;
+import com.zxd.www.sys.entity.Teacher;
+import com.zxd.www.sys.mapper.SysAdminMapper;
+import com.zxd.www.sys.mapper.TeacherMapper;
+import com.zxd.www.sys.service.TeacherService;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author Makonike
+ * @date 2021-10-15 21:11
+ **/
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+    @Autowired
+    private TeacherMapper teacherMapper;
+
+    @Autowired
+    private SysAdminMapper adminMapper;
+
+    /**
+     * 为管理员绑定teacher,并为管理员添加teacher角色
+     * @param teacher teacher
+     */
+    @Override
+    @Transactional(rollbackFor = {RuntimeException.class, Error.class})
+    public boolean save(Teacher teacher, Integer adminId) {
+        teacher.setAdminId(adminId);
+        adminMapper.bindTeacherToAdmin(adminId, 2);
+        return teacherMapper.save(teacher);
+    }
+
+    @Override
+    public boolean update(Teacher teacher) {
+        return teacherMapper.update(teacher);
+    }
+
+    @Override
+    public boolean deleteById(Integer teacherId) {
+        return teacherMapper.deleteById(teacherId);
+    }
+
+    @Override
+    public Teacher getByTeacherId(Integer teacherId) {
+        return teacherMapper.selectByTeacherId(teacherId);
+    }
+
+    @Override
+    public List<Teacher> getList() {
+        return teacherMapper.selectList();
+    }
+}

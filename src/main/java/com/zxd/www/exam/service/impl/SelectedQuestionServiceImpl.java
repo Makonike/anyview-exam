541d35b7355eceddc54347a68ffce7e4e3c7a9d9
Makonike <1694585033@qq.com>
2021-10-27 14:42:20 +0800

    chore(exam): 选择题目失败后回滚

--- src/main/java/com/zxd/www/exam/service/impl/SelectedQuestionServiceImpl.java
+++ src/main/java/com/zxd/www/exam/service/impl/SelectedQuestionServiceImpl.java
@@ -5,6 +5,7 @@ import com.zxd.www.exam.mapper.SelectedQuestionMapper;
 import com.zxd.www.exam.service.SelectedQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,6 +24,7 @@ public class SelectedQuestionServiceImpl implements SelectedQuestionService {
      * @param selectedQuestions 已选题目列表
      */
     @Override
+    @Transactional(rollbackFor = {RuntimeException.class, Error.class})
     public boolean add(List<SelectedQuestion> selectedQuestions) {
         questionMapper.deleteAll(selectedQuestions.get(0).getTableId());
         return questionMapper.insert(selectedQuestions);
